eaa12af7d8364429c1f5960fa2337546676ae572
asaihinata <hinataasaisacount@gmail.com>
2026-04-17 04:20:56 +0900

    feat
    feat
    Surveyグラフの作成開始

---
+++ src/widget/graph/_2D/survey.py
@@ -0,0 +1,31 @@
+from itertools import product
+from .._graphhelp import *
+class Survey(twoDElement):
+ def __init__(self,master,kw):
+  super().__init__(master,kw)
+  self.x=self._onearr(kw.get('x'))
+  self.y=self._manyarr(kw.get('y'))
+  self.logs=bols(kw.get('logs'),False)
+  self.label=self.labels(kw.get('label'))[0]
+  self.height=range_num(num0s(kw.get('height'),1),0,1,1)
+  self.align=listchose(kw.get('align'),['center','edge'])
+  self.plot(self.x,self.y,label=self.label,alpha=self.alpha,height=self.height,align=self.align,logs=self.logs)
+ def plot(self,x,y,label=None,alpha=1,height=1,align='center',logs=False):
+  self.clear()
+  for i,(xs,ys) in enumerate(product(x,y)):
+   self.graphdata.append(self.ax.barh(xs,ys,label=label[i],alpha=alpha,height=height,align=align,log=logs))
+  self._apply_labels(self.xlabel,self.ylabel)
+  self.legend()
+  self._adjustment()
+ def update(self,x=None,y=None,**kw):
+  self._updates(**kw)
+  if isinstance(x,NpArraytype):self.x=self._onearr(x)
+  if isinstance(y,NpArraytype):self.y=self._manyarr(y)
+  self.height=range_num(num0s(kw.get('height'),self.height),0,1,self.height)
+  self.align=listchose(kw.get('align'),['center','edge'],self.align)
+  self.logs=bols(kw.get('logs'),self.logs)
+  self.plot(self.x,self.y,label=self.label,alpha=self.alpha,height=self.height,align=self.align,log=self.logs)
+  self._redraw()
+ def get(self):return self.graphdata
+ def getx(self):return self.x
+ def gety(self):return self.y
