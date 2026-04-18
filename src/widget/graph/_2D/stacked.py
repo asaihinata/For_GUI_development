316ca7119c2eaca578bab34ad0b734630e38d737
asaihinata <hinataasaisacount@gmail.com>
2026-04-18 14:05:37 +0900

    fix
    fix
    Stacked,Stackedhのバグの修正

--- src/widget/graph/_2D/stacked.py
+++ src/widget/graph/_2D/stacked.py
@@ -14,19 +14,18 @@ class Stacked(twoDElement):
   self.plot(self.data,self.dataname,label=self.label,width=self.width)
  def plot(self,data,dataname,label=None,width=0.8):
   self.clear()
-  self.ax.set_ylim(0,100)
   self.graphdata=[self._survey(data,dataname,label,width=width)]
-  self.ax.yaxis.set_major_formatter(PercentFormatter(xmax=100))
   self._apply_labels(self.xlabel,self.ylabel)
   self._adjustment()
  def _survey(self,data:np.ndarray,dataname,label=None,width=0.8):
   data,lens,lisarr=data.T,len(dataname),[]
-  data_percent=data/data.sum(axis=0)*100
-  bottom=np.zeros(lens)
+  data_percent,bottom=data/data.sum(axis=0)*100,np.zeros(lens)
   for i,labels in enumerate(label):
    lisarr=self.ax.bar(dataname,data_percent[i],bottom=bottom,label=labels,width=width)
    bottom+=data_percent[i]
-  self.legend(ncols=lens)
+  self.legend(ncols=data.shape[0])
+  self.ax.set_ylim(0,100)
+  self.ax.yaxis.set_major_formatter(PercentFormatter(xmax=100))
   return lisarr
  def update(self,data=None,dataname=None,**kw):
   self._updates(**kw)
@@ -48,8 +47,8 @@ class Stackedh(twoDElement):
   if self.data.shape[0]!=self.dataname.shape[0]:
    raise ValueError('配列のエラー')
   self.label=self.labels(kw.get('label'),self.data.shape[1])[0]
-  self.width=range_num(num0s(kw.get('width'),0.8),0,1,0.8)
   self.anchor=self._anchor(kw.get('labelanchor'),(0,1))
+  self.labelplace=self._getlabelplace(self.anchor,kw.get('labelplace','lower left'))
   self.height=range_num(num0s(kw.get('height'),0.8),0,1,0.8)
   self.plot(self.data,self.dataname,label=self.label,height=self.height)
  def plot(self,data,dataname,label=None,height=0.8):
@@ -62,12 +61,11 @@ class Stackedh(twoDElement):
   self._adjustment()
  def _survey(self,data:np.ndarray,dataname,label=None,height=0.8):
   data,lens,lisarr=data.T,len(dataname),[]
-  data_percent=data/data.sum(axis=0)*100
-  left=np.zeros(lens)
+  data_percent,left=data/data.sum(axis=0)*100,np.zeros(lens)
   for i,labels in enumerate(label):
    lisarr=self.ax.barh(dataname,data_percent[i],left=left,label=labels,height=height)
    left+=data_percent[i]
-  self.legend(ncols=lens)
+  self.legend(ncols=data.shape[0])
   return lisarr
  def update(self,data=None,dataname=None,**kw):
   self._updates(**kw)
