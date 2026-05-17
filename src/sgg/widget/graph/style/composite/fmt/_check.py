c02969a2c458dbdfc699d84f1b50f6b374fde67f
asaihinata <hinataasaisacount@gmail.com>
2026-05-17 13:49:06 +0900

    refactor
    refactor
    src\sgg\widget\graph\style\composite\fmtの使用変更

--- src/sgg/widget/graph/style/composite/fmt/_check.py
+++ src/sgg/widget/graph/style/composite/fmt/_check.py
@@ -6,19 +6,19 @@ class fmtcolor:
   if not isinstance(arr,list|tuple|np.ndarray):self.arr=np.array([arr])
   elif isinstance(arr,list|tuple):self.arr=np.array(arr)
   else:self.arr=arr
-  self.arr=np.array([i in FMT_COLOR for i in np.nditer(self.arr)]).reshape(self.arr.shape)
+  self.arr=np.array([i for i in np.nditer(self.arr) if i in FMT_COLOR])
  def __iter__(self):return iter(self.arr)
 class fmtmarker:
  def __init__(self,arr:list|tuple|np.ndarray|str)->None:
   if not isinstance(arr,list|tuple|np.ndarray):self.arr=np.array([arr])
   elif isinstance(arr,list|tuple):self.arr=np.array(arr)
   else:self.arr=arr
-  self.arr=np.array([i in FMT_MARKER for i in np.nditer(self.arr)]).reshape(self.arr.shape)
+  self.arr=np.array([i for i in np.nditer(self.arr) if i in FMT_MARKER])
  def __iter__(self):return iter(self.arr)
 class fmtsolid:
  def __init__(self,arr:list|tuple|np.ndarray|str)->None:
   if not isinstance(arr,list|tuple|np.ndarray):self.arr=np.array([arr])
   elif isinstance(arr,list|tuple):self.arr=np.array(arr)
   else:self.arr=arr
-  self.arr=np.array([i in FMT_SOLID for i in np.nditer(self.arr)]).reshape(self.arr.shape)
+  self.arr=np.array([i for i in np.nditer(self.arr) if i in FMT_SOLID])
  def __iter__(self):return iter(self.arr)
