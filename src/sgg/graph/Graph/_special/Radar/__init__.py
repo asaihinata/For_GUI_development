2fc576c77327c9d88898b007614277e005d2adae
asaihinata <hinataasaisacount@gmail.com>
2026-05-23 04:46:57 +0900

    move
    move
    src/sgg/graph/Graph/_special/Radar/Radarplot.py -> src\sgg\graph\Graph\_special\Radar\__init__.py
    (上:コードの移動)

--- src/sgg/graph/Graph/_special/Radar/__init__.py
+++ src/sgg/graph/Graph/_special/Radar/__init__.py
@@ -1,1 +1,15 @@
-from .Radarplot import Radarplot
+from ...dev import *
+class Radarplot(RadarElement):
+ def __init__(self,master,kw):
+  super().__init__(master,kw)
+  self.plot(self.data)
+ def plot(self,data):
+  self.clear()
+  self.graphdata=[self.ax.plot(self.theta,d)for d in data]
+  self._adjustment()
+ def update(self,data=None,**kw):
+  self._updates(**kw)
+  if not isinstance(data,nListlike):self.data=self._dataarr(data,False).T
+  self.plot(self.data)
+  self._redraw()
+ def get(self):return self.graphdata
