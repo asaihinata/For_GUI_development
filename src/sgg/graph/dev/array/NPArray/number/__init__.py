b7d16707d25e881dea57143d548fe9068e0eda89
asaihinata <hinataasaisacount@gmail.com>
2026-05-24 07:12:12 +0900

    feat
    feat
    src\sgg\graph\dev\array\nparray\numberに合計,平均などの機能追加

--- src/sgg/graph/dev/array/NPArray/number/__init__.py
+++ src/sgg/graph/dev/array/NPArray/number/__init__.py
@@ -14,3 +14,52 @@ class NPNumber(NPArray):
   splices=shapes[-1]
   self.data=np.array([j*np.insert(j,0,1)[:-1] for i in range(0,len(datas),splices)for j in [datas[i:i+splices]]])
   return self
+ @property
+ def sum(self):return np.sum(self.data)
+ def asum(self,axis=None,dtype=None):return np.sum(self.data,axis=axis,dtype=dtype)
+ @property
+ def median(self):return np.median(self.data)
+ def amedian(self,axis=None):return np.median(self.data,axis=axis)
+ @property
+ def var(self):return np.var(self.data)
+ def avar(self,axis=None,dtype=None):return np.var(self.data,axis=axis,dtype=dtype)
+ @property
+ def max(self):return np.max(self.data)
+ def amax(self,axis=None):return np.amax(self.data,axis=axis)
+ @property
+ def min(self):return np.min(self.data)
+ def amin(self,axis=None):return np.amin(self.data,axis=axis)
+ @property
+ def mean(self):return np.mean(self.data)
+ def amean(self,axis=None,dtype=None):return np.mean(self.data,axis=axis,dtype=dtype)
+ @property
+ def std(self):return np.std(self.data)
+ def astd(self,axis=None,dtype=None):return np.std(self.data,axis=axis,dtype=dtype)
+ def percentile(self,q,axis=None,method='linear'):
+  if method not in [
+   'inverted_cdf',
+   'averaged_inverted_cdf',
+   'closest_observation',
+   'interpolated_inverted_cdf',
+   'hazen',
+   'weibull',
+   'linear',
+   'median_unbiased',
+   'normal_unbiased'
+  ]:
+   method='linear'
+  return np.percentile(self.data,q,axis=axis,method=method)
+ def quantile(self,q,axis=None,method='linear'):
+  if method not in [
+   'inverted_cdf',
+   'averaged_inverted_cdf',
+   'closest_observation',
+   'interpolated_inverted_cdf',
+   'hazen',
+   'weibull',
+   'linear',
+   'median_unbiased',
+   'normal_unbiased'
+  ]:
+   method='linear'
+  return np.quantile(self.data,q,axis=axis,method=method)
--- src/sgg/graph/dev/array/NPArray/number/__init__.pyi
+++ src/sgg/graph/dev/array/NPArray/number/__init__.pyi
@@ -1,3 +1,4 @@
+from typing import Literal
 from numpy import _ArrayT,ndarray
 from numpy._typing import DTypeLike
 from ..base import NPArray
@@ -11,3 +12,56 @@ dtype:DTypeLike|None=None
 )->None:...
  def cussum(self)->NPNumber:'''一つ前の元の値との和を求める。'''
  def cumprod(self)->NPNumber:'''一つ前の元の値との積を求める。'''
+ @property
+ def sum(self):...
+ def asum(self,axis:int|None=None,dtype:DTypeLike|None=None):...
+ @property
+ def median(self):...
+ def amedian(self,axis:int|None=None):...
+ @property
+ def var(self):...
+ def avar(self,axis:int|None=None,dtype:DTypeLike|None=None):...
+ @property
+ def max(self):...
+ def amax(self,axis:int|None=None):...
+ @property
+ def min(self):...
+ def amin(self,axis:int|None=None):...
+ @property
+ def mean(self):...
+ def amean(self,axis:int|None=None,dtype:DTypeLike|None=None):...
+ @property
+ def std(self):...
+ def astd(self,axis:int|None=None,dtype:DTypeLike|None=None):...
+ def percentile(
+self,
+q:tuple[int,...],
+axis:int|None=None,
+method:Literal[
+'inverted_cdf',
+'averaged_inverted_cdf',
+'closest_observation',
+'interpolated_inverted_cdf',
+'hazen',
+'weibull',
+'linear',
+'median_unbiased',
+'normal_unbiased'
+]='linear'
+):...
+ def quantile(
+self,
+q:tuple[int,...],
+axis:int|None=None,
+method:Literal[
+'inverted_cdf',
+'averaged_inverted_cdf',
+'closest_observation',
+'interpolated_inverted_cdf',
+'hazen',
+'weibull',
+'linear',
+'median_unbiased',
+'normal_unbiased'
+]='linear'
+):...
