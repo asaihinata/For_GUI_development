bf9c8cb8057e85f3d50eaa280d99bc2508329e1d
asaihinata <hinataasaisacount@gmail.com>
2026-05-26 08:49:59 +0900

    refactor
    refactor
    NPNumberに関する修正

--- src/sgg/graph/dev/array/NPArray/maths/__init__.py
+++ src/sgg/graph/dev/array/NPArray/maths/__init__.py
@@ -87,81 +87,58 @@ class NPNumber(NPArray):
  def pow2(self):return np.power(self.data,2,dtype=self.dtype)
  @property
  def deviation(self):return ((10/self.std)*(self.data-self.mean))+50
- def floor(self,digit=None):
-  if digit==None:
-   self.data=np.floor(self.data)
-  else:
-   pows=self.__digits(digit)
-   self.data=np.floor(self.data*pows)/pows
-  return self
- def trunc(self,digit=None):
-  if digit==None:
-   self.data=np.trunc(self.data)
-  else:
-   pows=self.__digits(digit)
-   self.data=np.trunc(self.data*pows)/pows
-  return self
- def ceil(self,digit=None):
-  if digit==None:
-   self.data=np.ceil(self.data)
-  else:
-   pows=self.__digits(digit)
-   self.data=np.ceil(self.data*pows)/pows
-  return self
- def round(self,digit=None):
-  if digit==None:
-   self.data=np.round(self.data)
-  else:
-   pows=self.__digits(digit)
-   self.data=np.round(self.data*pows)/pows
-  return self
- def afloor(self,digit=None):
-  if digit==None:return np.floor(self.data)
-  else:
-   pows=self.__digits(digit)
-   return np.floor(self.data*pows)/pows
- def atrunc(self,digit=None):
-  if digit==None:return np.trunc(self.data)
-  else:
-   pows=self.__digits(digit)
-   return np.trunc(self.data*pows)/pows
- def aceil(self,digit=None):
-  if digit==None:return np.ceil(self.data)
-  else:
-   pows=self.__digits(digit)
-   return np.ceil(self.data*pows)/pows
- def around(self,digit=None):
-  if digit==None:return np.round(self.data)
-  else:
-   pows=self.__digits(digit)
-   return np.round(self.data*pows)/pows
- def asum(self,axis=None,dtype=None):return np.sum(self.data,axis=axis,dtype=dtype)
- def amedian(self,axis=None):return np.median(self.data,axis=axis)
- def avar(self,axis=None,dtype=None):return np.var(self.data,axis=axis,dtype=dtype)
- def amax(self,axis=None):return np.amax(self.data,axis=axis)
- def amin(self,axis=None):return np.amin(self.data,axis=axis)
- def amean(self,axis=None,dtype=None):return np.mean(self.data,axis=axis,dtype=dtype)
- def astd(self,axis=None,dtype=None):return np.std(self.data,axis=axis,dtype=dtype)
- def amod(self,x):return np.mod(self.data,x,dtype=self.dtype)
- def adivmod(self,x):return np.divmod(self.data,x,dtype=self.dtype)
- def apow(self,x):return np.power(self.data,x,dtype=self.dtype)
- def apow2(self):return np.power(self.data,2,dtype=self.dtype)
- def alog(self):return np.log(self.data,dtype=self.dtype)
- def alogx(self,x):
+ @property
+ def log(self):return np.log(self.data,dtype=self.dtype)
+ @property
+ def log10(self):return np.log10(self.data,dtype=self.dtype)
+ @property
+ def log2(self):return np.log2(self.data,dtype=self.dtype)
+ @property
+ def log1p(self):return np.log1p(self.data,dtype=self.dtype)
+ @property
+ def degree(self):return np.degrees(self.data,dtype=self.dtype)
+ @property
+ def radian(self):return np.radians(self.data,dtype=self.dtype)
+ def logx(self,x):
   if not isinstance(x,int|float):
    raise TypeError('xには数値の型を指定してください')
   elif x<0:
    raise ValueError('xには0より大きい値を指定してください')
   return np.log(self.data,dtype=self.dtype)/np.log(x,dtype=self.dtype)
- def alog10(self):return np.log10(self.data,dtype=self.dtype)
- def alog2(self):return np.log2(self.data,dtype=self.dtype)
- def alog1p(self):return np.log1p(self.data,dtype=self.dtype)
+ def mod(self,x):return np.mod(self.data,x,dtype=self.dtype)
+ def divmod(self,x):return np.divmod(self.data,x,dtype=self.dtype)
  def pow(self,x):
   self.data=np.power(self.data,x,dtype=self.dtype)
   return self
- def sqrtroot(self,root):
+ def sqrt(self,root=2):
+  if root==0:
+   raise ZeroDivisionError('rootには0を指定できません')
   roots=1/root
   self.data=np.power(self.data,roots,dtype=self.dtype)
+  return self
+ def floor(self,digit=None):
+  if digit==None:self.data=np.floor(self.data)
+  else:
+   pows=self.__digits(digit)
+   self.data=np.floor(self.data*pows)/pows
+  return self
+ def trunc(self,digit=None):
+  if digit==None:self.data=np.trunc(self.data)
+  else:
+   pows=self.__digits(digit)
+   self.data=np.trunc(self.data*pows)/pows
+  return self
+ def ceil(self,digit=None):
+  if digit==None:self.data=np.ceil(self.data)
+  else:
+   pows=self.__digits(digit)
+   self.data=np.ceil(self.data*pows)/pows
+  return self
+ def round(self,digit=None):
+  if digit==None:self.data=np.round(self.data)
+  else:
+   pows=self.__digits(digit)
+   self.data=np.round(self.data*pows)/pows
   return self
  def cussum(self):
   datas,shapes=self._flatten()
@@ -180,30 +157,26 @@ class NPNumber(NPArray):
   if method not in method_list:method='linear'
   return np.quantile(self.data,q,axis=axis,method=method)
  def sturgesval(self,decimal=None,digit=None):
-  def floor(data,digit=None):
-   if digit==None:return np.floor(data)
-   else:
-    pows=self.__digits(digit)
-    return np.floor(data*pows)/pows
-  def trunc(data,digit=None):
-   if digit==None:return np.trunc(data)
-   else:
-    pows=self.__digits(digit)
-    return np.trunc(data*pows)/pows
-  def ceil(data,digit=None):
-   if digit==None:return np.ceil(data)
-   else:
-    pows=self.__digits(digit)
-    return np.ceil(data*pows)/pows
-  def round(data,digit=None):
-   if digit==None:return np.round(data)
-   else:
-    pows=self.__digits(digit)
-    return np.round(data*pows)/pows
   if decimal not in ['floor','trunc','ceil','round','none']:decimal='none'
   sturges=1+np.log2(self.size)
-  if decimal=='floor':return floor(sturges,digit)
-  elif decimal=='trunc':return trunc(sturges,digit)
-  elif decimal=='ceil':return ceil(sturges,digit)
-  elif decimal=='round':return round(sturges,digit)
+  if decimal=='floor':
+   if digit==None:return np.floor(sturges)
+   else:
+    pows=self.__digits(digit)
+    return np.floor(sturges*pows)/pows
+  elif decimal=='trunc':
+   if digit==None:return np.trunc(sturges)
+   else:
+    pows=self.__digits(digit)
+    return np.trunc(sturges*pows)/pows
+  elif decimal=='ceil':
+   if digit==None:return np.ceil(sturges)
+   else:
+    pows=self.__digits(digit)
+    return np.ceil(sturges*pows)/pows
+  elif decimal=='round':
+   if digit==None:return np.round(sturges)
+   else:
+    pows=self.__digits(digit)
+    return np.round(sturges*pows)/pows
   return sturges
