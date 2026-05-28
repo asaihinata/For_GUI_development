ce4564263ffb08a72c36f86c356391ed8484c9da
asaihinata <hinataasaisacount@gmail.com>
2026-05-28 12:59:45 +0900

    refactor
    refactor
    src\sgg\graph\dev\nparray\dev\dtypeの処理の変更

--- src/sgg/graph/dev/nparray/dev/dtype/__init__.py
+++ src/sgg/graph/dev/nparray/dev/dtype/__init__.py
@@ -13,58 +13,32 @@ __all__=[
 'uintDtype'
 ]
 class Arry:
- def __init__(self,arr):
+ def __init__(self,arr,dtype):
   if not isinstance(arr,np.ndarray):
    raise TypeError('np.ndarray型で指定してください')
   self.arr=arr
   self.dt=arr.dtype
+  self._set_dtype=dtype
+ @property
+ def bool(self):return np.issubdtype(self.dt,self._set_dtype)
+ def __bool__(self):return self.bool
 class integerDtype(Arry):
- def __init__(self,arr):super().__init__(arr)
- @property
- def bool(self):return np.issubdtype(self.dt,np.integer)
- def __bool__(self):return self.bool
+ def __init__(self,arr):super().__init__(arr,np.integer)
 class intDtype(Arry):
- def __init__(self,arr):super().__init__(arr)
- @property
- def bool(self):return np.issubdtype(self.dt,np.int_)
- def __bool__(self):return self.bool
+ def __init__(self,arr):super().__init__(arr,np.int_)
 class uintDtype(Arry):
- def __init__(self,arr):super().__init__(arr)
- @property
- def bool(self):return np.issubdtype(self.dt,np.uint)
- def __bool__(self):return self.bool
+ def __init__(self,arr):super().__init__(arr,np.uint)
 class floatDtype(Arry):
- def __init__(self,arr):super().__init__(arr)
- @property
- def bool(self):return np.issubdtype(self.dt,np.floating)
- def __bool__(self):return self.bool
+ def __init__(self,arr):super().__init__(arr,np.floating)
 class boolDtype(Arry):
- def __init__(self,arr):super().__init__(arr)
- @property
- def bool(self):return np.issubdtype(self.dt,np.bool_)
- def __bool__(self):return self.bool
+ def __init__(self,arr):super().__init__(arr,np.bool_)
 class complexDtype(Arry):
- def __init__(self,arr):super().__init__(arr)
- @property
- def bool(self):return np.issubdtype(self.dt,np.complexfloating)
- def __bool__(self):return self.bool
+ def __init__(self,arr):super().__init__(arr,np.complexfloating)
 class strDtype(Arry):
- def __init__(self,arr):super().__init__(arr)
- @property
- def bool(self):return np.issubdtype(self.dt,np.str_)
- def __bool__(self):return self.bool
+ def __init__(self,arr):super().__init__(arr,np.str_)
 class bytesDtype(Arry):
- def __init__(self,arr):super().__init__(arr)
- @property
- def bool(self):return np.issubdtype(self.dt,np.bytes_)
- def __bool__(self):return self.bool
+ def __init__(self,arr):super().__init__(arr,np.bytes_)
 class datetimeDtype(Arry):
- def __init__(self,arr):super().__init__(arr)
- @property
- def bool(self):return np.issubdtype(self.dt,np.datetime64)
- def __bool__(self):return self.bool
+ def __init__(self,arr):super().__init__(arr,np.datetime64)
 class timedeltaDtype(Arry):
- def __init__(self,arr):super().__init__(arr)
- @property
- def bool(self):return np.issubdtype(self.dt,np.timedelta64)
- def __bool__(self):return self.bool
+ def __init__(self,arr):super().__init__(arr,np.timedelta64)
