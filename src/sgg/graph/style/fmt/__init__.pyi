ecf9558fc561b6919434fdbf2cd6733248673948
asaihinata <hinataasaisacount@gmail.com>
2026-06-07 14:09:24 +0900

    refactor,chore
    refactor
    コードの修正
    
    chore
    docs\requirements.txtの修正

--- src/sgg/graph/style/fmt/__init__.pyi
+++ src/sgg/graph/style/fmt/__init__.pyi
@@ -1,9 +1,9 @@
 '''マーカー,線種,色を一度に設定するモジュール'''
 from collections.abc import Iterator
-from typing import Literal as L,TypeAlias
+from typing import Literal,TypeAlias
 import numpy as np
 __all__=['FMT','fmtstyle']
-MarkerType:TypeAlias=L[
+MarkerType:TypeAlias=Literal[
 '.',',','o',
 'v','^','<',
 '>','1','2',
@@ -16,8 +16,8 @@ class FMT:
  def __init__(
 self,
 marker:MarkerType|None=None,
-solid:L['-','--','-.',':']|None=None,
-color:L['b','g','r','c','m','y','k','w']|None=None
+solid:Literal['-','--','-.',':']|None=None,
+color:Literal['b','g','r','c','m','y','k','w']|None=None
 )->None:...
  def __str__(self)->str:...
  @property
@@ -26,7 +26,7 @@ class fmtstyle:
  def __init__(
 self,
 arr:np.typing.ArrayLike,
-style:L['color','marker','solid']='color'
+style:Literal['color','marker','solid']='color'
 )->None:...
  def __iter__(self)->Iterator[str]:...
  @property
