372a426bbe84c6c4b64b53bac0efbf0d9dd9de6b
asaihinata <hinataasaisacount@gmail.com>
2026-06-13 06:28:01 +0900

    chore
    chore
    細かな変更

--- pycachekill.py
+++ pycachekill.py
@@ -1,5 +1,4 @@
 from pathlib import Path
 from send2trash import send2trash
-target_dir=Path(__file__).parent
-for i in target_dir.glob('**/__pycache__'):send2trash(i)
+for i in Path(__file__).parent.glob('**/__pycache__'):send2trash(i)
 print('end')
