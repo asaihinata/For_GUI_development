551b9277756a88474523b27dbc9e4420017037ad
asaihinata <hinataasaisacount@gmail.com>
2026-07-18 05:20:22 +0900

    fix
    fix
    経験的累積分布関数のバグの修正

--- src/sgg/graph/Graph/_2D/ecdf/__init__.py
+++ src/sgg/graph/Graph/_2D/ecdf/__init__.py
@@ -33,6 +33,8 @@ class Ecdf(twoElement):
         alpha,
     ):
         self.clear()
+        if data.ndim==1:
+            data=np.array([data])
         self.graphdata = [
             self.ax.ecdf(
                 ds,
