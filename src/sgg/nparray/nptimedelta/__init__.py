570166e41a386dec49a35d2c737de421c162a656
asaihinata <hinataasaisacount@gmail.com>
2026-09-01 06:20:14 +0900

    feat
    feat
    NPTimedeltaにarangeの機能の追加

--- src/sgg/nparray/nptimedelta/__init__.py
+++ src/sgg/nparray/nptimedelta/__init__.py
@@ -6,7 +6,7 @@ import numpy as np
 
 from sgg.dev import _tonparray
 
-from ..dev import _ArrayCommonMixin, _tm64_unit
+from ..dev import _ArrayCommonMixin, _tm64_unit,_get_tm64_unit
 
 __all__ = ["NPTimedelta"]
 
@@ -185,6 +185,14 @@ class NPTimedelta(_ArrayCommonMixin):
         if dtype.kind == "M":
             return np.asarray(self, dtype).view(type(self))
         return self.__array__(dtype, copy=copy)
+
+    @classmethod
+    def arange(cls, start,/, stop=None, step=1, dtype=None):
+        result = np.asarray(
+            np.arange(start, stop, step=step,dtype=_tm64_unit(dtype))
+        ).view(cls)
+        result._dtype = result.dtype
+        return result
 
     @property
     def dtypeunit(self):
